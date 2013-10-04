493842b0c48c8ec0dc03e9f312006c9d3fa9b2b4
alexpods <aleksey.podskrebyshev@gmail.com>
2013-10-05 00:45:08 +0400

    Fix service and action creation

--- dist/ApiJS.js
+++ dist/ApiJS.js
@@ -39,14 +39,18 @@ clazz('Factory', function(meta) {
         },
         methods: {
             createService: function(name, metaData) {
-                return meta
-                    .processor(this.clazz.const('META')('SERVICE'))
-                    .process(this.getServiceClazz().create({ name: name }), metaData);
+                var service = this.getServiceClazz().create({ name: name });
+
+                meta.processor(this.clazz.const('META')('SERVICE')).process(service, metaData);
+
+                return service;
             },
             createAction: function(name, metaData) {
-                return meta
-                    .processor(this.clazz.const('META')('ACTION'))
-                    .process(this.getActionClazz().create({ name: name}), metaData);
+                var action = this.getActionClazz().create({ name: name});
+
+                meta.processor(this.clazz.const('META')('ACTION')).process(action, metaData);
+
+                return action;
             }
         }
     }
